11fb7f549990388cec55a575619355c1abf9cfb2
AnamKhan04 <khananam231273@gmail.com>
2023-07-03 23:34:32 +0530

    multiplication

--- Calculator.py
+++ Calculator.py
@@ -2,9 +2,12 @@ def add(a,b):
     return a+b
 def sub(a,b):
     return a-b
+def mul(a,b):
+    return a*b
 
 num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
 
 print(f"The sum of {num1} and {num2} is: ",add(num1,num2))
 print(f"The substraction of {num1} and {num2} is: ",sub(num1,num2))
+print(f"The multiplication of {num1} and {num2} is: ",mul(num1,num2))
