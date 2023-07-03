ef8c0cd2216ba4423ab8d12f847c4ce5fe62e450
AnamKhan04 <khananam231273@gmail.com>
2023-07-03 23:25:41 +0530

    addition

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+# opr=input("Enter operator: ")
+
+print(f"The sum of {num1} and {num2} is: ",add(num1,num2))
