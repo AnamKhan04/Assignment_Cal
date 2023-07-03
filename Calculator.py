9b852fbfb8f25e235da7fb8288122afe85a0b718
AnamKhan04 <khananam231273@gmail.com>
2023-07-03 23:32:16 +0530

    substraction

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,10 @@
 def add(a,b):
     return a+b
+def sub(a,b):
+    return a-b
 
 num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
-# opr=input("Enter operator: ")
 
 print(f"The sum of {num1} and {num2} is: ",add(num1,num2))
+print(f"The substraction of {num1} and {num2} is: ",sub(num1,num2))
