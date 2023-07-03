a98158b4829608187a023018f00bd74f3b44ecef
AnamKhan04 <khananam231273@gmail.com>
2023-07-03 23:37:03 +0530

    division

--- Calculator.py
+++ Calculator.py
@@ -4,6 +4,11 @@ def sub(a,b):
     return a-b
 def mul(a,b):
     return a*b
+def div(a,b):
+    try:
+        return a/b
+    except:
+        print("Invalid value of b, Please enter valid input")
 
 num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
@@ -11,3 +16,4 @@ num2=int(input("Enter second number: "))
 print(f"The sum of {num1} and {num2} is: ",add(num1,num2))
 print(f"The substraction of {num1} and {num2} is: ",sub(num1,num2))
 print(f"The multiplication of {num1} and {num2} is: ",mul(num1,num2))
+print(f"The division of {num1} and {num2} is: ",div(num1,num2))
